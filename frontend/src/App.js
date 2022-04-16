de22e6933f61e37ff16927e5241d901df4321527
Lander Joshua Vitug <lander.vitug@gmail.com>
2022-04-15 23:50:57 -0400

    reinstall to react v17 and setup the grid css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,8 +3,23 @@ import React, { useState } from 'react'
 const App = () => {
 
   return (
-    <>
-    </>
+    <div className='container'>
+      <div className='item-title'>
+        <p>Yo</p>
+      </div>
+      <div className='item-about'>
+        <p>Yo</p>
+      </div>
+      <div className='item-left'>
+        <p>Yo</p>
+      </div>
+      <div className='item-center'>
+        <p>Yo</p>
+      </div>
+      <div className='item-right'>
+        <p>Yo</p>
+      </div>
+    </div>
   )
 }
 
